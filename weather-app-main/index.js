152493758e2603d998a020b9aad3598f5422ff37
unknown <Everett.Horton95@yahoo.com>
2026-01-14 15:52:17 -0500

    new commit

--- weather-app-main/index.js
+++ weather-app-main/index.js
@@ -1,1 +1,14 @@
+function searchButton() {
+    document.getElementById("Search").innerHTML = "This button has been clicked";
+    alert("Search CLicked");
+}
+
+
+const btn = document.getElementById("searchButton");
+btn.addEventListener("click", searchButton);
+
+
+
+
+
 const API_KEY = "c13a15d0f9c61d66257040dc14e1bbc6";
