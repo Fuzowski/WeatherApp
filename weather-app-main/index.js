40d0fd7542fef1b40a42711bbf28544dc41f2846
unknown <Everett.Horton95@yahoo.com>
2026-01-16 01:28:40 -0500

    trying again

--- weather-app-main/index.js
+++ weather-app-main/index.js
@@ -1,14 +1,51 @@
 function searchButton() {
-    document.getElementById("Search").innerHTML = "This button has been clicked";
-    alert("Search CLicked");
+  document.getElementById("Search").innerHTML = "This button has been clicked";
+  alert("Search CLicked");
 }
-
 
 const btn = document.getElementById("searchButton");
 btn.addEventListener("click", searchButton);
 
+// setInterval(()=>{
+// let classN = document.body.className;
+// if(classN==='light'){
+//     document.body.className = 'dark'
+// }else{
+//     document.body.className = 'light'
 
-
-
+// }
+// },2000)
 
 const API_KEY = "c13a15d0f9c61d66257040dc14e1bbc6";
+const baseUrl = `https://api.openweathermap.org/data/2.5/forecast?
+lat=44.34
+&
+units=imperial
+&
+lon=10.99
+&
+appid=${API_KEY}
+`;
+
+fetch(baseUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    renderHourlyForecast(data.list.slice(0, 8));
+  });
+
+function renderHourlyForecast(list) {
+  const hourlyForecast = document.getElementById("hourlyForecast");
+  for (let i = 0; i < list.length; i++) {
+    hourlyForecast.innerHTML += renderHourItem(list[i]);
+  }
+}
+
+function renderHourItem(hourSlot) {
+  return `
+        <div class="hourItem flex">
+            <div>${hourSlot.dt_txt}</div>
+            <div>${hourSlot.main.temp} F</div>
+        </div>
+    `;
+}
